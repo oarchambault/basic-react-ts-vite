857abcb8f718472e9caf3439947d4c6d727feb2c
Olivier Archambault <oarchambault@gmail.com>
2022-09-03 18:12:47 +0200

    non working commit

---
+++ src/components/auth/AuthStatus.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./Auth";
+
+
+export function AuthStatus() {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  if (!auth.user) {
+    return <p>You are not logged in.</p>;
+  }
+
+  return (
+    <p>
+      Welcome {auth.user}!{" "}
+      <button
+        onClick={() => {
+          auth.signout(() => navigate("/"));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  );
+}
